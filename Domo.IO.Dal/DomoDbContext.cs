b45cb78931e242359c9b3c94d9dd4447bc284c75
pierrick pobelle <pierrick.pobelle@gmail.com>
2016-03-23 23:27:15 +0100

    second

---
+++ Domo.IO.Dal/DomoDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domo.IO.Dal
+{
+    public class DomoDbContext : DbContext
+    {
+        //public IDbSet<SadTv> Tvs { get; set; }
+    
+        public DomoDbContext() : base("DomoDbConnection")
+        {
+
+        }
+    }
+}
